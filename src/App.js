595d2d69ebe365340d1e0bdd6055661918d44599
Ashley Chen <ashleyc2219@gmail.com>
2022-02-21 11:59:02 +0800

    if/else 寫成function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { useState } from 'react'
 function App() {
   const [total, setTotal] = useState(0)
+  const checkAndOutput = (value) => {
+    if (value > 4) {
+      return <p>總數大於4</p>
+    } else {
+      return <p>總數小於4</p>
+    }
+  }
+
   return (
     <>
       <h1 onClick={() => setTotal(total + 1)}>{total}</h1>
-      {total > 3 ? <p>數字大於3</p> : <p>數字小於3</p>}
+      {checkAndOutput(total)}
     </>
   )
 }
