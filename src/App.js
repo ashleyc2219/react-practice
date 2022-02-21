0e687a76774b27f5d3f527e50dc8cb748bd0dd93
Ashley Chen <ashleyc2219@gmail.com>
2022-02-21 15:46:02 +0800

    react-share 套件

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Parent from './components/Parent'
+import SocialShare from './components/SocialShare'
 function App() {
   return (
     <>
       <Parent />
+      <br />
+      <SocialShare url="http://localhost:3000/" round="true" />
     </>
   )
 }
