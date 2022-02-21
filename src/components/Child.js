3d5c35b9cc62030d371cb36bff89b73883c3b751
Ashley Chen <ashleyc2219@gmail.com>
2022-02-21 15:34:52 +0800

    proptypes 套件

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 function Child(props) {
   console.log(props)
   return (
@@ -6,10 +7,15 @@ function Child(props) {
     </>
   )
 }
-
+// 設定預設props，父元件沒傳值進來時使用
 Child.defaultProps = {
   name: 'Guest',
   text: 'Please Log In...',
 }
 
+// 使用套件，限制傳進來得props資料類型
+Child.PropTypes = {
+  name: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+}
 export default Child
