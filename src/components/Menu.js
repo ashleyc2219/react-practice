9ff2b161fefe222facc8baa3c3e9320bd5de56ae
Ashley Chen <ashleyc2219@gmail.com>
2022-02-21 21:54:49 +0800

    加註解

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 function Menu() {
+  // useState 儲存被點到的item的index，設定初始值為-1
   const [activeIndex, setActiveIndex] = useState(-1)
+  // 將menu的選項存成array，方便取用index值跟文字
   const menuItems = ['首頁', '關於我們', '產品']
   return (
+    // 用map將array變成，JSX 樣板，
     <>
-      {/* onClick={() => setClassText(classText + 1)}
-      className={classText > 0 ? 'active' : ''} */}
       <ul>
         {menuItems.map((value, index) => {
+            {/* 被點到的li會觸發onclick將activeIndex設成該item的index */}
           return (
             <li
               key={index}
@@ -15,6 +17,7 @@ function Menu() {
                 setActiveIndex(index)
               }}
             >
+              {/* 如果index 和被點到的li index相同就加上class=active*/}
               <a href="#/" className={activeIndex === index ? 'active' : ''}>
                 {value}
               </a>
