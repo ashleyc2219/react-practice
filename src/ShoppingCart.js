ba78bb847ffbebe9aa16aec26e34aec82620f361
Ashley Chen <ashleyc2219@gmail.com>
2022-02-24 11:12:18 +0800

    購物車 元件

---
+++ src/ShoppingCart.js
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import CartList from './components/CartList'
+import CartSummary from './components/CartSummary'
+import HW2 from './components/HW2'
+//   商品物件 資訊
+const product = [
+  {
+    id: 1,
+    name: '黑色 T-shirt',
+    category: 'Shirt',
+    image: 'https://i.imgur.com/1GrakTl.jpg',
+    price: 300,
+  },
+]
+function App() {
+  const [count, setCount] = useState(1)
+  return (
+    <>
+    <HW2/>
+      {/* <div className="card">
+        <div className="row">
+          <CartList product={product} count={count} setCount={setCount} />
+          <CartSummary
+            totalCount={count}
+            totalPrice={count * product[0].price}
+          />
+        </div>
+      </div> */}
+    </>
+  )
+}
+export default App
