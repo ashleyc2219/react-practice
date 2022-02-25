d86f8738bfa94568da3bdfac5e2755aa56313ebe
Ashley Chen <ashleyc2219@gmail.com>
2022-02-25 15:35:24 +0800

    模擬 + prevState

--- src/components/FuncComLifeCycle.js
+++ src/components/FuncComLifeCycle.js
@@ -1,8 +1,25 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
+
+function usePrevious(value) {
+  const ref = useRef()
+  useEffect(() => {
+    ref.current = value
+  })
+  return ref.current
+}
 
 function FuncComLifeCycle(props) {
   const [total, setTotal] = useState(0)
+
+  //   要得到前一個total
+  const prevTotal = usePrevious(total)
+
+  //   v3
+  const [didMount, setDidMount] = useState(false)
+
+  //   v4
+  //   ref 的特性可以觀測到，是否有真實DOM，如果有didMountRef.current就會有值
+  const didMountRef = useRef(false)
 
   // didMount
   useEffect(() => {
@@ -21,15 +38,60 @@ function FuncComLifeCycle(props) {
     }
   }, [])
 
+  // -----
+
+  // didUpdate(用初始值0判斷) 100% 模擬
+  // 會回到初始值無法判斷出來
+  useEffect(() => {
+    // 用初始值0判斷
+    if (total !== 0) console.log('didUpdate v2')
+  }, [total])
+
+  //----
+
+  // didMount時設置didMount旗標為true
+  useEffect(() => {
+    setDidMount(true)
+  }, [])
+
+  // didUpdate(用didMount旗標判斷) 100% 模擬
+  useEffect(() => {
+    // 用初始值0判斷
+    if (didMount) console.log('didUpdate v3')
+  }, [total])
+
+  //---
+
+  // didUpdate(用didMountRef參照旗標判斷) 100% 模擬
+  useEffect(() => {
+    if (didMountRef.current) {
+      // didUpdate
+      console.log('didUpdate v4')
+
+      // 得到前一個狀態值
+      console.log('total=', total, ' previous total=', prevTotal)
+    } else {
+      didMountRef.current = true
+    }
+  }, [total])
+
   return (
     <>
-      <h1
+      <h1>{total}</h1>
+      <button
         onClick={() => {
           setTotal(total + 1)
         }}
       >
-        {total}
-      </h1>
+        +1
+      </button>
+      <button
+        onClick={() => {
+          setTotal(total - 1)
+        }}
+      >
+        -1
+      </button>
     </>
   )
 }
